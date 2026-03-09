590f1c8567d300cedfea9eecd9b6c37d5b5c5991
MateusKasuya <MateusKasuya@users.noreply.github.com>
2026-03-09 18:27:10 +0000

    deploy: 0cbbb3ad2d28a334afb15212c8c0e401f9eebaa5

---
+++ compiled/dbt_nba/models/marts/dim_players.sql
@@ -0,0 +1,28 @@
+
+
+WITH dim_players AS (
+
+    SELECT
+        p.player_id,
+        p.player_name,
+        p.position,
+        p.team_id,
+        p.team_name,
+        p.team_abbreviation,
+        s.age,
+        g.last_game_text,
+        ir.status,
+        ir.description,
+        ir.return_date,
+        CURRENT_TIMESTAMP() AS loaded_at
+    FROM
+        `smartbetting-dados`.`nba`.`stg_active_players` AS p
+    LEFT JOIN
+        `smartbetting-dados`.`nba`.`stg_season_averages_general_base` AS s
+        ON p.player_id = s.player_id
+    LEFT JOIN `smartbetting-dados`.`nba`.`int_game_player_stats_last_game_text` AS g ON p.player_id = g.player_id
+    LEFT JOIN `smartbetting-dados`.`nba`.`stg_player_injuries` AS ir
+        ON p.player_id = ir.player_id
+)
+
+SELECT * FROM dim_players
---
+++ compiled/dbt_nba/models/staging/stg_team_season_averages_general_advanced.sql
@@ -0,0 +1,24 @@
+
+
+WITH source_data AS (
+    SELECT * FROM `smartbetting-dados`.`nba`.`raw_team_season_averages_general_advanced`
+),
+
+cleaned_data AS (
+    SELECT
+        CAST(team.id AS INT64) AS team_id,
+        CAST(season AS INT64) AS season,
+        season_type,
+        CAST(stats.off_rating AS FLOAT64) AS team_offensive_rating,
+        CAST(stats.def_rating AS FLOAT64) AS team_defensive_rating,
+        COALESCE(
+            SAFE_CAST(stats.net_rating AS FLOAT64),
+            CAST(stats.off_rating AS FLOAT64) - CAST(stats.def_rating AS FLOAT64)
+        ) AS team_net_rating,
+        SAFE_CAST(stats.poss AS FLOAT64) AS team_possessions,
+        SAFE_CAST(stats.pace AS FLOAT64) AS team_pace,
+        CURRENT_TIMESTAMP() AS loaded_at
+    FROM source_data
+)
+
+SELECT * FROM cleaned_data
